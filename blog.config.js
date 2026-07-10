604ae435be221d8a2216f88a9149a42b2239b5b6
Cleo <67538726+cleo3xd@users.noreply.github.com>
2026-07-10 23:02:48 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -17,7 +17,7 @@ const BLOG = {
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'A1tism', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '休恋逝水 早悟兰因', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || '人生没有标准答案', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://a1tism.com', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '个人同人站', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
